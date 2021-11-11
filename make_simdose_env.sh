ed7b01bf1257db90c073ee028b64b107656f8ae6
dbalick <dbalick@users.noreply.github.com>
2021-11-10 19:40:25 -0500

    Add files via upload

---
+++ make_simdose_env.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+## --------------------------------------------------
+##  To create the simDoSe environment,
+##      use the following command:
+## --------------------------------------------------
+conda create -n simdose python=2.7 numpy scipy matplotlib pandas
+
+
+
+## --------------------------------------------------
+##  To run simDoSe, first use the follwing command
+##      to activate the `simdose' the environment
+## --------------------------------------------------
+# conda activate simdose
+
+
+
+## --------------------------------------------------
+##  When you are finished, use the following command
+##      to deactivate the `simdose' the environment
+## --------------------------------------------------
+# conda deactivate
+
+
+
+## --------------------------------------------------
+##  If you try to run simDoSe and you find there is
+##     a missing package, use the following commands
+## to install that package in the simdose environment
+## --------------------------------------------------
+# conda activate simdose
+# conda install <missing package>
+
+
+
